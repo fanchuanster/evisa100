7f9f5935db7aaab6876c7dfe0f0c57e0e518a635
Wen Dong <wen.dong@microfocus.com>
2018-12-12 20:17:42 +0800

    passport displayed all

--- miniprogram/pages/passportInfo/passportInfo.js
+++ miniprogram/pages/passportInfo/passportInfo.js
@@ -6,8 +6,29 @@ Page({
    * Page initial data
    */
   data: {
+    passport_no:'',
+    country: '',
+    type:'',
+
+    name_cn:'',
+    name:'',
+    sex:'',
+    birth_date: '',
+
+    birth_place:'',
+    birth_place_raw:'',
+    issue_date:'',
+    issue_place:'',
+    issue_place_raw:'',
+    expiry_date:'',
+
     authority: '',
-    birth_date: ''
+
+    person_id:'',
+
+    line0:'',
+    line1:'',
+    success:''
   },
 
   /**
@@ -15,8 +36,25 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
+      passport_no: app.globalData.passportInfo.passport_no,
+      country: app.globalData.passportInfo.country,
+
+      type: app.globalData.passportInfo.type,
+      name_cn: app.globalData.passportInfo.name_cn,
+      name: app.globalData.passportInfo.name,
+      sex: app.globalData.passportInfo.sex,
+      birth_date: app.globalData.passportInfo.birth_date,
+      birth_place: app.globalData.passportInfo.birth_place,
+      birth_place_raw: app.globalData.passportInfo.birth_place_raw,
+      issue_date: app.globalData.passportInfo.issue_date,
+      issue_place: app.globalData.passportInfo.issue_place,
+      issue_place_raw: app.globalData.passportInfo.issue_place_raw,
+      expiry_date: app.globalData.passportInfo.expiry_date,
       authority: app.globalData.passportInfo.authority,
-      birth_date: app.globalData.passportInfo.birth_date
+
+      person_id: app.globalData.passportInfo.person_id,
+      birth_date: app.globalData.passportInfo.birth_date,
+      success: app.globalData.passportInfo.success      
     })
   },
 

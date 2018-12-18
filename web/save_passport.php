a25590c5f3fbb3ebbfc42d0fa8e60e6bc829293a
Wen Dong <wen.dong@microfocus.com>
2018-12-18 13:44:18 +0800

    tst

--- web/save_passport.php
+++ web/save_passport.php
@@ -7,22 +7,22 @@
  $input = file_get_contents("php://input");
  var_dump($input);
  
- $data = json_decode(file_get_contents('php://input'), true);
+ $data = json_decode($input);
  
  $savepassportstr = "insert into passport(passport_no, name, name_cn, sex,birth_date,birth_place,birth_place_raw,country,type,authority, issue_date, issue_place,issue_place_raw,expiry_date) ".
 "values('".$data->passport_no."','".
 $data->name."','".
 $data->name_cn."','".
-$data->sex."','".
+$data->sex."',".
 "CAST('".$data->birth_date."' AS DATE)".",'".
 $data->birth_place."','".
 $data->birth_place_raw."','".
 $data->country."','".
 $data->type."','".
-$data->authority."','".
+$data->authority."',".
 "CAST('".$data->issue_date."' AS DATE)".",'".
 $data->issue_place."','".
-$data->issue_place_raw."','".
+$data->issue_place_raw."',".
 "CAST('".$data->expiry_date."' AS DATE))";
   print($savepassportstr);
 

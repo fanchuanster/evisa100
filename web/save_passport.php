400c2213fa2923cbd9dabf838fc735da2ab303be
Wen Dong <wen.dong@microfocus.com>
2018-12-18 19:58:41 +0800

    test

--- web/save_passport.php
+++ web/save_passport.php
@@ -6,7 +6,7 @@
  
  $input = file_get_contents("php://input");
  $data = json_decode($input);
-  
+   
  if ($data->id) {
 	// update it.
 	$updatestr = "update passport set ".
@@ -29,7 +29,7 @@
 	$mysqli->query($updatestr);
  } else {
 	 // insert it.
-	$savepassportstr = "insert into passport(passport_no, name, name_cn, sex,birth_date,birth_place,birth_place_raw,country,type,authority, issue_date, issue_place,issue_place_raw,expiry_date) ".
+	$insertStr = "insert into passport(passport_no, name, name_cn, sex,birth_date,birth_place,birth_place_raw,country,type,authority, issue_date, issue_place,issue_place_raw,expiry_date) ".
 	"values('".$data->passport_no."','".
 	$data->name."','".
 	$data->name_cn."','".
@@ -45,7 +45,7 @@
 	$data->issue_place_raw."',".
 	"CAST('".$data->expiry_date."' AS DATE))";
 
-	$mysqli->query($savepassportstr);
+	$mysqli->query($insertStr);
 	
 	// return inserted id to client.
 	$lastId = 'no';

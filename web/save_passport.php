8632d90b83e2818ae61a209a1559e546cb34e5af
Wen Dong <wen.dong@microfocus.com>
2018-12-18 13:42:18 +0800

    tst

--- web/save_passport.php
+++ web/save_passport.php
@@ -3,24 +3,28 @@
  $mysqli = new mysqli("localhost", "root", "root", "evisa");
  $mysqli->query("set NAMES 'utf8'");
  $mysqli->query("SET CHARACTER SET utf8");
- var_dump('good');
- print('good');
- // $savePassportStr = "
-// insert into passport(passport_no, name, name_cn, sex,birth_date,birth_place,birth_place_raw,country,type,authority, issue_date, issue_place,issue_place_raw,expiry_date) 
-// values(".$_POST['passport_no'].",".
-// $_POST['name'].",".
-// $_POST['name_cn'].",".
-// $_POST['sex'].",".
-// "CAST(".$_POST['birth_date']." AS DATE)".",".
-// $_POST['birth_place'].",".
-// $_POST['birth_place_raw'].",".
-// $_POST['country'].",".
-// $_POST['type'].",".
-// $_POST['authority'].",".
-// "CAST(".$_POST['issue_date']." AS DATE)".",".
-// $_POST['issue_place'].",".
-// $_POST['issue_place_raw'].",".
-// "CAST(".$_POST['expiry_date']." AS DATE)");
+ 
+ $input = file_get_contents("php://input");
+ var_dump($input);
+ 
+ $data = json_decode(file_get_contents('php://input'), true);
+ 
+ $savepassportstr = "insert into passport(passport_no, name, name_cn, sex,birth_date,birth_place,birth_place_raw,country,type,authority, issue_date, issue_place,issue_place_raw,expiry_date) ".
+"values('".$data->passport_no."','".
+$data->name."','".
+$data->name_cn."','".
+$data->sex."','".
+"CAST('".$data->birth_date."' AS DATE)".",'".
+$data->birth_place."','".
+$data->birth_place_raw."','".
+$data->country."','".
+$data->type."','".
+$data->authority."','".
+"CAST('".$data->issue_date."' AS DATE)".",'".
+$data->issue_place."','".
+$data->issue_place_raw."','".
+"CAST('".$data->expiry_date."' AS DATE))";
+  print($savepassportstr);
 
   // $mysqli->query($savePassportStr);
 

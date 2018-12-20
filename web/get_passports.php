01ce7e2e383615c1084019b0777af6057f6c229c
Wen Dong <wen.dong@microfocus.com>
2018-12-20 11:03:42 +0800

    fix - change _GET() TO $_GET[]

--- web/get_passports.php
+++ web/get_passports.php
@@ -6,7 +6,7 @@
  
   $querystr = "SELECT 'passport_no', passport_no, 'openid', openid, 'data', data from passport ".
 			"WHERE ";
-  if (isset($_GET['id']) {
+  if (isset($_GET['id'])) {
 	  $querystr .= "id=".$_GET['id'];
   } else if (isset($_GET['passport_no'])) {
 	  $querystr .= "passport_no='".$_GET['passport_no']."'";

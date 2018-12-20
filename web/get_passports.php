31522c01f4d737843cbc2a840a96b1aed992ac21
Wen Dong <wen.dong@microfocus.com>
2018-12-20 10:49:07 +0800

    add get passports php file

---
+++ web/get_passports.php
@@ -0,0 +1,29 @@
+<?php
+
+  $mysqli = new mysqli("localhost", "root", "root", "evisa");
+  //$mysqli->query("set NAMES 'utf8'");
+  //$mysqli->query("SET CHARACTER SET utf8");
+ 
+  $querystr = "SELECT 'passport_no', passport_no, 'openid', openid, 'data', data from passport ".
+			"WHERE ";
+  if (isset(_GET('id')) {
+	  $querystr .= "id="._GET('id');
+  } else if (isset(_GET('passport_no'))) {
+	  $querystr .= "passport_no='"._GET('passport_no')."'";
+  } else if (isset(_GET('openid'))) {
+	  $querystr .= "openid='"._GET('openid')."'";
+  }
+  
+  var_dump($querystr);
+  
+  $result = $mysqli->query($updatestr);
+  
+  $records = array();
+  while ($row = $mysqli->fetch_assoc($result)) {
+	  $records[] = $row;
+  }
+
+  print(json_encode($records)); 
+ }
+
+?>

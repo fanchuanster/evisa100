f226953c94e58b3d6f90babfba083a7586f7340f
Wen Dong <wen.dong@microfocus.com>
2018-12-28 18:28:47 +0800

    test

--- web/get_openid.php
+++ web/get_openid.php
@@ -3,7 +3,7 @@
 	$appid = $_GET['appid'];
 	$appsecret = $_GET['appsecret'];
 	$code = $_GET['code'];
-	$weixin =  file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$appsecret."&code=".$code."&grant_type=authorization_code");
+	$weixin =  file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&appsecret=".$appsecret."&code=".$code."&grant_type=authorization_code");
 	var_dump($weixin);
 	$jsondecode = json_decode($weixin);
 	var_dump($jsondecode);

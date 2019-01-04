d5e5e1b821dfeb407ed8b10afd89aed8e0fa1132
Wen Dong <wen.dong@microfocus.com>
2019-01-04 17:24:21 +0800

    test

--- web/get_application.php
+++ web/get_application.php
@@ -4,7 +4,7 @@
   $mysqli->query("set NAMES 'utf8'");
   $mysqli->query("SET CHARACTER SET utf8");
  
-  $querystr = "SELECT 'id', id, 'passport_id', passport_id,'to_country', to_country, 'purpose', purpose, 'entry_date', entry_date, 'departure_date', departure_date, 'other_info', other_info`".
+  $querystr = "SELECT 'id', id, 'passport_id', passport_id,'to_country', to_country, 'entry_date', entry_date, 'departure_date', departure_date, 'other_info', other_info`".
 			" FROM application".
 			" WHERE current_status=0 LIMIT 1";
   

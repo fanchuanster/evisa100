e5b23e41ae206b58abcfaca28937e00e4c5980f0
Wen Dong <wen.dong@microfocus.com>
2019-01-04 17:26:00 +0800

    test

--- web/get_application.php
+++ web/get_application.php
@@ -7,9 +7,7 @@
   $querystr = "SELECT 'id', id, 'passport_id', passport_id,'to_country', to_country, 'entry_date', entry_date, 'departure_date', departure_date, 'other_info', other_info`".
 			" FROM application".
 			" WHERE current_status=0 LIMIT 1";
-  
-  var_dump($querystr);
-  
+    
   $result = $mysqli->query($querystr);
   
   $row = $result->fetch_assoc();

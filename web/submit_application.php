0a8521a784227279e027841a30b9477893612adc
Wen Dong <wen.dong@microfocus.com>
2019-01-04 16:07:59 +0800

    test

--- web/submit_application.php
+++ web/submit_application.php
@@ -4,34 +4,34 @@
  
  $input = file_get_contents("php://input");
   var_dump($input);
- $travelInfo = json_decode($input);
+ // $travelInfo = json_decode($input);
 
- if ($travelInfo->id) {
-	// update it.
-	$updatestr = "update application set ".
-	"passport_id=".$travelInfo->passport_id.",".
-	"to_country='".$travelInfo->to_country."',".
-	"purpose='".$travelInfo->purpose."',".
-	"entry_date=CAST('".$travelInfo->entry_date."' AS DATE),".
-	"departure_date=CAST('".$travelInfo->departure_date."' AS DATE)".
-	" WHERE id='".$travelInfo->id."'";
+ // if ($travelInfo->id) {
+	// // update it.
+	// $updatestr = "update application set ".
+	// "passport_id=".$travelInfo->passport_id.",".
+	// "to_country='".$travelInfo->to_country."',".
+	// "purpose='".$travelInfo->purpose."',".
+	// "entry_date=CAST('".$travelInfo->entry_date."' AS DATE),".
+	// "departure_date=CAST('".$travelInfo->departure_date."' AS DATE)".
+	// " WHERE id='".$travelInfo->id."'";
         
-	// var_dump($updatestr);
+	// // var_dump($updatestr);
 
-	$mysqli->query($updatestr);
- } else {
-	 // insert it.
-	$insertStr = "insert into application(passport_id, to_country, purpose, entry_date, departure_date) ".
-	"values(".$travelInfo->passport_id.",'".
-	$travelInfo->to_country."','".
-	$travelInfo->purpose."',".
-	"CAST('".$travelInfo->entry_date."' AS DATE),".
-	"CAST('".$travelInfo->departure_date."' AS DATE))";
+	// $mysqli->query($updatestr);
+ // } else {
+	 // // insert it.
+	// $insertStr = "insert into application(passport_id, to_country, purpose, entry_date, departure_date) ".
+	// "values(".$travelInfo->passport_id.",'".
+	// $travelInfo->to_country."','".
+	// $travelInfo->purpose."',".
+	// "CAST('".$travelInfo->entry_date."' AS DATE),".
+	// "CAST('".$travelInfo->departure_date."' AS DATE))";
          
-    // var_dump($insertStr);
-	$mysqli->query($insertStr);
+    // // var_dump($insertStr);
+	// $mysqli->query($insertStr);
 
-	print('{"id":'.$mysqli->insert_id;.'}'); 
+	// print('{"id":'.$mysqli->insert_id;.'}'); 
  }
 
 ?>

083830ef45d75ce7a85e1da84c0ae3f567fe7af4
Wen Dong <wen.dong@microfocus.com>
2019-01-06 20:32:17 +0800

    test

--- web/get_application.php
+++ web/get_application.php
@@ -15,5 +15,5 @@
 	  $records[] = $row;
   }
 
-  print(json_encode($records));
+  print(stripslashes(json_encode($records)));
 ?>
--- web/get_passports.php
+++ web/get_passports.php
@@ -23,5 +23,5 @@
 	  $records[] = $row;
   }
 
-  print(json_encode($records));
+  print(stripslashes(json_encode($records)));
 ?>

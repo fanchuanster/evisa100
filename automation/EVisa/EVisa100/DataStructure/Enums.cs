e0c689d47a87e0442552036b4d2fae846e9c0ad7
Wen Dong <wen.dong@microfocus.com>
2019-01-09 17:03:57 +0800

    add entrypoints

---
+++ automation/EVisa/EVisa100/DataStructure/Enums.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EVisa100.Enums
+{
+    public enum TransportationBy
+    {
+        Air = 0,
+        Road,
+        Ship
+    }
+
+    public enum Purpose
+    {
+        Tourism = 0,
+        Business,
+        Education,
+        Employment,
+        Medical,
+        Religion,
+        Settlement,
+        Others
+    }
+}
---
+++ web/save_entrypoints.php
@@ -0,0 +1,30 @@
+<?php
+
+ $mysqli = new mysqli("localhost", "root", "root", "evisa");
+ $mysqli->query("set NAMES 'utf8'");
+ $mysqli->query("SET CHARACTER SET utf8");
+ 
+ $input = file_get_contents("php://input");
+ $entrypoint = json_decode($input);
+
+ if ($entrypoint->id) {
+	// update it.
+	$updatestr = "update entrypoint set ".
+	"country='".$entrypoint->country."'".
+	"data='".addslashes($entrypoint->data)."'".
+	" WHERE id='".$entrypoint->id."'";
+
+	$mysqli->query($updatestr);
+ } else {
+	 // insert it.
+	$insertStr = "insert into entrypoint(country, data) ".
+	"values('".$entrypoint->country."','".
+	addslashes($entrypoint->data)."')";
+	
+	$mysqli->query($insertStr);
+	
+	// return inserted id to client.
+	print('{"id":'.$mysqli->insert_id.'}');
+ }
+
+?>

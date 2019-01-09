3fa9601d6f4248519a6623f11633a3f67dcabb2c
Wen Dong <wen.dong@microfocus.com>
2019-01-09 12:09:14 +0800

    add citiesmanager and utility.cs

---
+++ automation/EVisa/EVisa100/CitiesManager.cs
@@ -0,0 +1,32 @@
+﻿using EVisa100.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace EVisa100
+{
+    public static class CitiesManager
+    {
+        static List<City> cities;
+        static CitiesManager()
+        {
+            JavaScriptSerializer converter = new JavaScriptSerializer();
+            using (StreamReader r = new StreamReader("cities.json"))
+            {
+                var json = r.ReadToEnd();
+                cities = converter.Deserialize<List<City>>(json);
+            }
+        }
+        public static string GetCityPinyin(string name_cn)
+        {
+            var city = cities.Find(c => c.name == name_cn);
+            if (city == null) return string.Empty;
+
+            return city.pinyin;
+        }
+    }
+}

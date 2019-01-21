e80486b48a0784044e086fbba1a6424e97512b7a
Wen Dong <wen.dong@microfocus.com>
2019-01-21 17:17:42 +0800

    cleanup - miniprogram; src/iview

--- miniprogram/config.js
+++
@@ -1,9 +0,0 @@
-var fileHost = 'https://evisa.oss-cn-shanghai.aliyuncs.com'
-var config = {
-  //aliyun OSS config
-  uploadImageUrl: `${fileHost}`, //default target directory as root.
-  accesskey: 'NyNOmcGHgyKX5vHUtn9lv7M1vCJWL3',
-  accessid: 'LTAIQMDDOG5SOtsh',
-  timeout: 87600 // policy valid timespan.
-};
-module.exports = config

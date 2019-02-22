d3e299898a6b2dfe38ec67a4124eba8174c7fda4
Wen Dong <wen.dong@microfocus.com>
2019-02-22 11:03:39 +0800

    update db schema other_info changed to data in application

--- web/submit_application.php
+++ web/submit_application.php
@@ -6,9 +6,9 @@
  
  $input = file_get_contents("php://input");
  $travelInfo = json_decode($input);
- $otherInfo = json_encode($travelInfo->other_info);
+ $data = json_encode($travelInfo->data);
  
- $otherInfo = str_replace("\u", "\\u", $otherInfo);
+ $data = str_replace("\u", "\\u", $data);
  
  if ($travelInfo->id) {
 	// update it.
@@ -16,7 +16,7 @@
 	"passport_id=".$travelInfo->passport_id.",'".
 	"to_country'='".$travelInfo->to_country."','".
 	"purpose'='".$travelInfo->purpose."','".
-	"otherInfo'='".addslashes($otherInfo)."','".
+	"data'='".addslashes($data)."','".
 	"entry_date'=CAST('".$travelInfo->entry_date."' AS DATE),'".
 	"departure_date'=CAST('".$travelInfo->departure_date."' AS DATE)".
 	" WHERE id='".$travelInfo->id."'";
@@ -26,13 +26,13 @@
 	$mysqli->query($updatestr);
  } else {
 	 // insert it.
-	$insertStr = "insert into application(passport_id, to_country, purpose, entry_date, departure_date, other_info) ".
+	$insertStr = "insert into application(passport_id, to_country, purpose, entry_date, departure_date, data) ".
 	"values(".$travelInfo->passport_id.",'".
 	$travelInfo->to_country."','".
 	$travelInfo->purpose."',".
 	"CAST('".$travelInfo->entry_date."' AS DATE),".
 	"CAST('".$travelInfo->departure_date."' AS DATE),'".
-	addslashes($otherInfo)."')";
+	addslashes($data)."')";
 	
 	$mysqli->query($insertStr);
 

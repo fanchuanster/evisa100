83dc35e61fca63e0069fac44398a54bfc465515a
Wen Dong <wen.dong@microfocus.com>
2019-03-02 21:55:01 +0800

    wot - control visibility by fields

--- wepy/src/mixins/test.js
+++ wepy/src/mixins/test.js
@@ -2,13 +2,18 @@ import wepy from 'wepy'
 
 export default class testMixin extends wepy.mixin {
   data = {
-    mixin: 'This is mixin data.'
+    mixin: 'This is mixin data.',
+    countryFields: { ke: {'passport_file': 1, 'photo_file':1, 'id_front_file':1} }
   }
   methods = {
     tap () {
       this.mixin = 'mixin data was changed'
       console.log('mixin method tap')
     }
+  }
+
+  getFieldNames(country) {
+    return this.countryFields[country]
   }
 
   onShow() {

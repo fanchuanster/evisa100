ae4336fe80f43e156b2fe00fe9252055a11620fa
Wen Dong <wen.dong@microfocus.com>
2019-03-07 17:44:22 +0800

    remove unnecessary fields

--- wepy/src/mixins/test.js
+++ wepy/src/mixins/test.js
@@ -7,8 +7,8 @@ export default class testMixin extends wepy.mixin {
       ke: {
         passport_file:1, photo_file:1, id_front_file:1,
         nationality_cn:1, name_cn:1, name:1, sex:1, birth_date:1, birth_place_cn:1, birth_place:1,
-        job_type:1, job_comp_cn:1, job_comp:1, job_title_cn:1, job_title:1,
-        passport_no:1, authority:1, issue_place_cn:1, issue_place:1, issue_date:1, expiry_date:1,
+        job_type:1, job_comp_cn:0, job_comp:0, job_title_cn:1, job_title:1,
+        passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
         father_name:1, mother_name:1, region:0, address_cn:1, address:1, phone:1, email:1,
         purpose:1, entry_date:1, departure_date:1, by:1,
         return_to_domicile:1, been_other_country:1, ever_been_to:1, ever_denied:1,

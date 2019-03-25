6488b86491f4c312633cde58c3d31cd31bf83792
Wen Dong <wen.dong@microfocus.com>
2019-03-26 06:25:26 +0800

    wepy upgraded to 1.7.2 from 1.6.x; get_countries php added

---
+++ web/get_countries.php
@@ -0,0 +1,21 @@
+<?php
+
+  $mysqli = new mysqli("localhost", "root", "root", "evisa");
+  $mysqli->query("set NAMES 'utf8'");
+  $mysqli->query("SET CHARACTER SET utf8");
+   
+  $querystr = "SELECT 'id', id,"
+  ." 'name', name, 'name_cn', name_cn, 'name_short', name_short, continent, continent"
+  ." FROM country";
+    
+  // var_dump($querystr);
+    
+  $result = $mysqli->query($querystr);
+  
+  $records = array();
+  while ($row = $result->fetch_assoc()) {
+	  $records[] = $row;
+  }
+
+  print((json_encode($records)));
+?>

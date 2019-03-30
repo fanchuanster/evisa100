08db903c6a50b1e960912f27d9e90e650d4d131c
Wen Dong <wen.dong@microfocus.com>
2019-03-30 21:47:24 +0800

    add files from evisa100

--- wepy-mall/src/utils/util.js
+++ wepy-mall/src/utils/util.js
@@ -119,6 +119,34 @@ function convertStarArray(score) {
   }
   return arr
 }
+dateFromString = function(str) {
+	if (!str) {
+		return str
+	}
+	str = str.replace(/\D/g, '')
+	if (str.length < 6) {
+		return str
+	}
+	let dd = str.substr(0, 2)
+	str = str.substr(2)
+	let yyyy = str.substr(str.length - 4)
+	if (str.length === 6) {
+		return yyyy + '-' + 1 + '-' + dd
+	} else {
+		let mm = str.substr(0, str.length - 4)
+		return yyyy + '-' + mm + '-' + dd
+	}
+}
+
+splitCnEnName = function(data, prop) {
+	if (data[prop]) {
+	  var places = data[prop].split('/', 2)
+	  if (places.length === 2) {
+		data[prop + '_cn'] = places[0]
+		data[prop] = places[1]
+	  }
+	}
+}
 module.exports = {
   getCurrentTime: getCurrentTime,
   objLength: objLength,
@@ -130,5 +158,7 @@ module.exports = {
   div: div,
   mul: mul,
   accAdd: accAdd,
-  convertStarArray: convertStarArray
+  convertStarArray: convertStarArray,
+  dateFromString: dateFromString,
+  splitCnEnName: splitCnEnName
 }

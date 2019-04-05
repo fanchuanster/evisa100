99ea0cf3d271d0e5d29dd7e98413da62a723da4f
Wen Dong <wen.dong@microfocus.com>
2019-04-06 06:29:21 +0800

    test

--- web/wx_php_sdk_v3.0.9/wxpay/index.php
+++ web/wx_php_sdk_v3.0.9/wxpay/index.php
@@ -56,7 +56,7 @@
 	<div align="center">
         <ul>
             <li style="background-color:#FF7F24"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/jsapi.php">JSAPI支付</a></li>
-            <li style="background-color:#698B22"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/wx_php/example/micropay.php">刷卡支付</a></li>
+            <li style="background-color:#698B22"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/micropay.php">刷卡支付</a></li>
             <li style="background-color:#8B6914"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/native.php">扫码支付</a></li>
             <li style="background-color:#CDCD00"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/orderquery.php">订单查询</a></li>
             <li style="background-color:#CD3278"><a href="http://<?php echo $_SERVER['SERVER_NAME']?>/wx_sdk/wxpay/refund.php">订单退款</a></li>

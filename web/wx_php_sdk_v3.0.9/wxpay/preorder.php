fce4def29e74f32255a540e70962bdbfe0639690
Wen Dong <wen.dong@microfocus.com>
2019-04-06 09:51:01 +0800

    add preorder.php

---
+++ web/wx_php_sdk_v3.0.9/wxpay/preorder.php
@@ -0,0 +1,49 @@
+<?php 
+/**
+*
+* example目录下为简单的支付样例，仅能用于搭建快速体验微信支付使用
+* 样例的作用仅限于指导如何使用sdk，在安全上面仅做了简单处理， 复制使用样例代码时请慎重
+* 请勿直接直接使用样例对外提供服务
+* 
+**/
+require_once "../lib/WxPay.Api.php";
+require_once "WxPay.JsApiPay.php";
+require_once "WxPay.Config.php";
+require_once 'log.php';
+
+//初始化日志
+$logHandler= new CLogFileHandler("../logs/".date('Y-m-d').'.log');
+$log = Log::Init($logHandler, 15);
+
+//打印输出数组信息
+function printf_info($data)
+{
+    foreach($data as $key=>$value){
+        echo "<font color='#00ff55;'>$key</font> :  ".htmlspecialchars($value, ENT_QUOTES)." <br/>";
+    }
+}
+
+//①、获取用户openid
+try{
+
+	$tools = new JsApiPay();
+	$openId = $_GET['openid'];
+
+	//②、统一下单
+	$input = new WxPayUnifiedOrder();
+	$input->SetBody("test");
+	$input->SetAttach("test");
+	$input->SetOut_trade_no("sdkphp".date("YmdHis"));
+	$input->SetTotal_fee("1");
+	$input->SetTime_start(date("YmdHis"));
+	$input->SetTime_expire(date("YmdHis", time() + 600));
+	$input->SetGoods_tag("test");
+	$input->SetNotify_url("https://php.evisa100.com/notify.php");
+	$input->SetTrade_type("JSAPI");
+	$input->SetOpenid($openId);
+	$config = new WxPayConfig();
+	$order = WxPayApi::unifiedOrder($config, $input);
+	var_dump($order);
+} catch(Exception $e) {
+	Log::ERROR(json_encode($e));
+}

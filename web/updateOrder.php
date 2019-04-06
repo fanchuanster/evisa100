770f9d78ede674057f231e3ddb9265059f9bef68
Wen Dong <wen.dong@microfocus.com>
2019-04-06 13:05:31 +0800

    test

---
+++ web/updateOrder.php
@@ -0,0 +1,22 @@
+<?php
+
+ $mysqli = new mysqli("localhost", "root", "root", "evisa");
+ $mysqli->query("set NAMES 'utf8'");
+ $mysqli->query("SET CHARACTER SET utf8");
+ 
+ $input = file_get_contents("php://input");
+ $json = json_decode($input);
+ 
+ // $data = str_replace("\u", "\\u", $data);
+ 
+ if ($json->id) {
+	// update it.
+	$updatestr = "update application set ".
+	"status=".$json->status." ".
+	" WHERE id=".$json->id;
+        
+	// var_dump($updatestr);
+
+	$mysqli->query($updatestr);
+ }
+?>

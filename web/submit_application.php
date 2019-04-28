75c64e8c01c1e0270ff728860d8318d341d10e92
Wen Dong <wen.dong@microfocus.com>
2019-04-28 10:40:34 +0800

    fix update application

--- web/submit_application.php
+++ web/submit_application.php
@@ -5,30 +5,28 @@
  $mysqli->query("SET CHARACTER SET utf8");
  
  $input = file_get_contents("php://input");
- $travelInfo = json_decode($input);
- $data = json_encode($travelInfo->data);
+ $application = json_decode($input);
+ $data = json_encode($application->data);
  
  // $data = str_replace("\u", "\\u", $data);
  
- if ($travelInfo->id) {
+ if ($application->id) {
 	// update it.
 	$updatestr = "update application set ".
-	"passport_id=".$travelInfo->passport_id.",".
-	"status=".$travelInfo->status.",".
-	"entry_date=CAST('".$travelInfo->entry_date."' AS DATE),".
-	"departure_date=CAST('".$travelInfo->departure_date."' AS DATE)".
-	"data='".addslashes($data)."',".
-	" WHERE id=".$travelInfo->id;
-        
-	var_dump($updatestr);
+	"passport_id=".$application->passport_id.",".
+	"status=".$application->status.",".
+	"entry_date=CAST('".$application->entry_date."' AS DATE),".
+	"departure_date=CAST('".$application->departure_date."' AS DATE),".
+	"data='".addslashes($data)."'".
+	" WHERE id=".$application->id;
 
 	$mysqli->query($updatestr);
  } else {
 	 // insert it.
 	$insertStr = "insert into application(passport_id, entry_date, departure_date, data) ".
-	"values(".$travelInfo->passport_id.",".
-	"CAST('".$travelInfo->entry_date."' AS DATE),".
-	"CAST('".$travelInfo->departure_date."' AS DATE),'".
+	"values(".$application->passport_id.",".
+	"CAST('".$application->entry_date."' AS DATE),".
+	"CAST('".$application->departure_date."' AS DATE),'".
 	addslashes($data)."')";
 	
 	// var_dump($insertStr);

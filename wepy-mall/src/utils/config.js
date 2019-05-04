105caf3259454aa2e90ddfe494d9f63177476fee
fanchuanster@gmail.com <fanchuanster@gmail.com>
2019-05-04 20:50:00 +0800

    update status for reviewed

--- wepy-mall/src/utils/config.js
+++ wepy-mall/src/utils/config.js
@@ -14,7 +14,7 @@ var config = {
       father_name:1, father_name_cn:1, mother_name:1, mother_name_cn:1, region:1, address_cn:1, address:1, phone:1, email:1,
       purpose:1, entry_date:1, departure_date:1, by:1, entry_point:1,
       return_to_domicile:1, been_other_country:1, ever_been_to:1, ever_denied:1,
-      ever_denied_by_other:1, ever_convicted:1, destination: 1
+      ever_denied_by_other:1, ever_convicted:1, destination: 0
     },
     ke: {
       passport_file:1, photo_file:1, other_file:0, id_front_file:1,
@@ -33,7 +33,7 @@ var config = {
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
       father_name:1, mother_name:1, region:1, address_cn:1, address:1, phone:1, email:1,
       purpose:1, entry_date:1, departure_date:1, by:1,
-      ever_been_to:1, destination: 1
+      ever_been_to:1, destination: 0
     },
     zm: {
       passport_file:1, photo_file:1,
@@ -42,7 +42,7 @@ var config = {
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
       father_name:1, mother_name:1, region:1, address_cn:1, address:1, phone:1, email:1,
       purpose:1, entry_date:1, departure_date:1, by:1,
-      ever_been_to:1, destination: 1
+      ever_been_to:1
     },
     tz: {
       passport_file:1, photo_file:1,
@@ -51,7 +51,7 @@ var config = {
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
       father_name:1, mother_name:1, region:1, address_cn:1, address:1, phone:1, email:1,
       purpose:1, entry_date:1, departure_date:1, by:1,
-      ever_been_to:1, destination: 1
+      ever_been_to:1
     }
   },
 

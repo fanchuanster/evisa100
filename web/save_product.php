49651d13378203b5783c228cb6853331e6d9473a
Wen Dong <wen.dong@microfocus.com>
2019-06-10 17:42:12 +0800

    test

--- web/save_product.php
+++ web/save_product.php
@@ -21,7 +21,7 @@
 	$insertStr = "insert into product(store_id, country_id, data) ".
 	"values(".$product->store_id.",".
 	$product->country_id.",'".
-	addslashes($data)."')";
+	($data)."')";
 	var_dump($insertStr);
 	$mysqli->query($insertStr);
 	

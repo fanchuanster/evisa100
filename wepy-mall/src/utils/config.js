5cd394f4e8b26bca28b208ea50114c6cb70625f5
Wen Dong <wen.dong@microfocus.com>
2019-06-14 11:39:41 +0800

    remove id_front_file field

--- wepy-mall/src/utils/config.js
+++ wepy-mall/src/utils/config.js
@@ -7,7 +7,7 @@ var config = {
   adminOpenid: 'opini5GNX-N6JKq6aqutfPHCxUDc',
   countryFields: {
     all: {
-      passport_file:1, photo_file:1, other_file:0, id_front_file:0, hotel_letter:0,
+      passport_file:1, photo_file:1, other_file:0, hotel_letter:0,
       country_cn:1, name_cn:1, name:1, sex:1, marital_status:1, birth_date:1, birth_place_cn:1, birth_place:1,
       job_type:1, job_comp_cn:0, job_comp:0, job_title_cn:1, job_title:1,
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
@@ -17,7 +17,7 @@ var config = {
       ever_denied_by_other:1, ever_convicted:1, destination: 0
     },
     ke: {
-      passport_file:1, photo_file:1, other_file:0, id_front_file:1,
+      passport_file:1, photo_file:1, other_file:0,
       country_cn:0, name_cn:1, name:1, sex:1, birth_date:1, birth_place_cn:1, birth_place:1,
       job_type:1, job_comp_cn:0, job_comp:0, job_title_cn:1, job_title:1,
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,

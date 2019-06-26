04ea304947240a1e993043929c8c23d27df2b3ed
Wen Dong <wen.dong@microfocus.com>
2019-06-26 17:56:58 +0800

    wot

--- wepy-mall/src/utils/config.js
+++ wepy-mall/src/utils/config.js
@@ -19,7 +19,7 @@ var config = {
     },
     ke: {
       passport_file:1, photo_file:1, other_file:1,
-      country_cn:0, name_cn:1, name:1, sex:1, birth_date:1, birth_place_cn:1, birth_place:1,
+      country_cn:0, name_cn:1, name:1, sex:1, birth_date:1, birth_place_cn:0, birth_place:1,
       job_type:1, job_comp_cn:0, job_comp:0, job_title_cn:1, job_title:1,
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
       father_name:1, mother_name:1, region:1, address_cn:1, address:1, phone:1, email:1,
@@ -56,7 +56,7 @@ var config = {
     },
     pak: {
       passport_file:1, photo_file:1,
-      country_cn:0, name_cn:1, name:1, sex:1, marital_status:1, birth_date:1, birth_place_cn:1, birth_place:1,
+      country_cn:0, name_cn:1, name:1, sex:1, marital_status:1, birth_date:1, birth_place_cn:0, birth_place:1,
       job_type:1, job_comp_cn:1, job_comp:0, job_title_cn:1, job_title:1,
       job_comp_address_cn:1, job_comp_address:1, job_comp_email:1, job_comp_tel:1, job_comp_join_date:1,  
       passport_no:1, authority:1, issue_place:1, issue_date:1, expiry_date:1,
